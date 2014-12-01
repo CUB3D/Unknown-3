efd109af97da5cbb2f638a2faeccbdce52eb1c3b
CUB3D <callumthom1@gmail.com>
2014-12-01 18:37:07 +0000

    Added: +UI2D.line
    Fixed:
    *All methods in UI2D take doubles for posisions and sizes

--- project/src/call/game/geom/UI2D.java
+++ project/src/call/game/geom/UI2D.java
@@ -8,7 +8,7 @@ import call.game.main.Unknown;
 
 public class UI2D
 {
-	public static void rect(int x, int y, int width, int height, int col)
+	public static void rect(double x, double y, double width, double height, int col)
 	{
 		GL2 gl = Unknown.getGL();
 
@@ -21,20 +21,20 @@ public class UI2D
 
 		gl.glColor4f(red / 255, green / 255, blue / 255, alpha / 255);
 
-		gl.glVertex2f(x, y);
-		gl.glVertex2f(x, y + height);
-		gl.glVertex2f(x + width, y + height);
-		gl.glVertex2f(x + width, y);
+		gl.glVertex2d(x, y);
+		gl.glVertex2d(x, y + height);
+		gl.glVertex2d(x + width, y + height);
+		gl.glVertex2d(x + width, y);
 
 		gl.glEnd();
 	}
 
-	public static void square(int x, int y, int size, int col)
+	public static void square(double x, double y, double size, int col)
 	{
 		rect(x, y, size, size, col);
 	}
 
-	public static void triangle(int x, int y, int width, int height, int col)
+	public static void triangle(double x, double y, double width, double height, int col)
 	{
 		GL2 gl = Unknown.getGL();
 
@@ -47,14 +47,14 @@ public class UI2D
 
 		gl.glColor4f(red / 255, green / 255, blue / 255, alpha / 255);
 
-		gl.glVertex2f(x + width / 2, y + height);
-		gl.glVertex2f(x, y);
-		gl.glVertex2f(x + width, y);
+		gl.glVertex2d(x + width / 2, y + height);
+		gl.glVertex2d(x, y);
+		gl.glVertex2d(x + width, y);
 
 		gl.glEnd();
 	}
 
-	public static void polygon(int x, int y, Point[] verts)
+	public static void polygon(double x, double y, Point[] verts)
 	{
 		GL2 gl = Unknown.getGL();
 
@@ -66,6 +66,27 @@ public class UI2D
 		for(Point d : verts)
 			gl.glVertex2d(x + d.getX(), y + d.getY());
 
+		gl.glEnd();
+	}
+	
+	public static void line(double x, double y, double x2, double y2, float width, int col)
+	{
+		GL2 gl = Unknown.getGL();
+		
+		int blue = (col >> 0) & 0xFF;
+		int green = (col >> 8) & 0xFF;
+		int red = (col >> 16) & 0xFF;
+		int alpha = (col >> 24) & 0xFF;
+
+		gl.glBegin(GL2.GL_LINES);
+		
+		gl.glLineWidth(width);
+		
+		gl.glColor4f(red / 255, green / 255, blue / 255, alpha / 255);
+		
+		gl.glVertex2d(x, y);
+		gl.glVertex2d(x2, y2);
+		
 		gl.glEnd();
 	}
 }
